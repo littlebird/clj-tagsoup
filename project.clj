6f70e33a567a77bab9ea0db235a36a100e3c1189
Justin Glenn Smith <noisesmith@gmail.com>
2016-01-14 12:51:34 -0800

    bumping version and moving to our clojars repo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tagsoup "0.3.0"
+(defproject littlebird-aviary/clj-tagsoup "0.3.1"
   :description "A HTML parser for Clojure."
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
                  [org.clojure/data.xml "0.0.3"]
